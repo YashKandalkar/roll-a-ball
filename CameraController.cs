e4a290e70ee609f1b64c750bb51d058a77b493e8
Yash <yashsk.yk@gmail.com>
2018-03-12 16:15:36 +0530

    First Commit

---
+++ CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    public Camera camera;
+
+   void Start()
+    {
+        offset = camera.transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        camera.transform.position = player.transform.position + offset;
+    }
+}
---
+++ PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    public Text CountText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    void FixedUpdate()
+    {
+        float moveH = Input.GetAxis("Horizontal");
+        float moveV = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveH, 0.0f, moveV);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUps"))
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        CountText.text = "Count: " + count.ToString();
+        if (count >= 10)
+        {
+            winText.text = "YOU WIN!!";
+        }
+    }
+}
